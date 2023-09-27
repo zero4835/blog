a8bfdd20b89fa6d1b83e5376e6fef1b809543c78
meteor <zz5120937@gmail.com>
2023-09-27 23:50:45 +0800

    backup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">Logo</div>
+        <img src="./images/black-cat.png" width="25" height="25" alt="dog"></img>
+        <div className="links">
+          <Link className="link" to="https://www.facebook.com/"><h6>Facebook</h6></Link>
+          <Link className="link" to="https://zero4835.github.io/" ><h6>Blog</h6></Link>
+          <Link className="link" to="https://github.com/zero4835" ><h6>Github</h6></Link>
+          <span>John</span>
+          <span>Login</span>
+          <span className="write">
+            <Link to="/write" >Write</Link>
+          </span>
+        </div>
+
+      </div>
     </div>
   )
 }
