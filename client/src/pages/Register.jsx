74aaf60b4720a82601d4f2b3a5fddc549f515976
Natsusaka <zz5120937@gmail.com>
2023-10-14 14:33:18 +0800

    First commit

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder='username'/>
-        <input type="email" placeholder='email'/>
-        <input type="password" placeholder='password'/>
-        <button >Register</button>
+        <input required type="text" placeholder="username" />
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button>Register</button>
         <p>username or password Error!</p>
-        <span> Don't you have an account? <Link to='/login'>Register</Link></span>
+        <span>
+          {" "}
+          Don't you have an account? <Link to="/login">Register</Link>
+        </span>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
