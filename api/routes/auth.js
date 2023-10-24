c67afb259c7504df43448526bb550c1f51ed2942
Natsusaka <zz5120937@gmail.com>
2023-10-24 16:00:49 +0800

    feat: add proxy

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { login, logout, register } from "../cotrollers/auth.js";
 
 const router = express.Router()
+
+router.post("/register", register)
+router.post("/login", login)
+router.post("/logout", logout)
+
+
 export default router
 
