7c3a4db1c4972ed62c040e974e8036f115c77b43
Natsusaka <zz5120937@gmail.com>
2023-10-24 17:25:47 +0800

    feat: remove {}

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -44,7 +44,6 @@ const Login = () => {
         <button onClick={handleSubmit}>Login</button>
         {err && <p>{err}</p>}
         <span>
-          {" "}
           Don't you have an account? <Link to="/register">Register</Link>
         </span>
       </form>
