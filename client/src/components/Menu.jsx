4aa5f62853036a2e7583f2415bf08da77e2df177
Natsusaka <zz5120937@gmail.com>
2023-10-25 23:33:38 +0800

    feat: modify indentation

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -30,7 +30,7 @@ export const Menu = () => {
   return (
     <div className="menu">
       <h1>Other post you make like</h1>
-      {posts?.map((post)=>(
+      {posts?.map((post) => (
         <div className="post" key={post.id}>
           <img src={post.img} alt="" />
           <h2>{post.title}</h2>
