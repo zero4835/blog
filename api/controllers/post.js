9eb56e409ab3a6e8c3d81aa866791edc4da2fd3d
Natsusaka <zz5120937@gmail.com>
2023-10-25 16:15:41 +0800

    feat: modify folder name

---
+++ api/controllers/post.js
@@ -0,0 +1,34 @@
+import { db } from "../db.js";
+
+export const getPosts = (req, res) => {
+  const q = req.query.cat
+    ? "SELECT * FROM posts WHERE cat=?"
+    : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.status(500).send(err);
+
+    return res.status(200).json(data);
+  });
+};
+
+export const getPost = (req, res) => {
+  const q =
+    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    return res.status(200).json(data[0]);
+  });
+};
+
+export const addPost = (req, res) => {
+  res.json("from controller");
+};
+export const deletePost = (req, res) => {
+  res.json("from controller");
+};
+export const updatePost = (req, res) => {
+  res.json("from controller");
+};
