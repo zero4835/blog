fc3fb3a6e1e72c10a185df897698c17feb4229e9
Natsusaka <zz5120937@gmail.com>
2023-10-27 00:10:18 +0800

    feat: add Link state {post }

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -56,7 +56,7 @@ const Single = () => {
             <p>Posted {moment(post.date).fromNow()}</p>
           </div>
           {currentUser && currentUser.username === post.username && (<div className="edit">
-            <Link to={`/write?edit=2`}>
+            <Link to={`/write?edit=2`} state={post}>
               <img src={Edit} />
             </Link>
             <img onClick={handleDelete} src={Delete} />
