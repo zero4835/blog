d5dd6671d25e9b0155f5126114ec264856cb3c93
Natsusaka <zz5120937@gmail.com>
2023-10-26 15:54:41 +0800

    feat: create multer storage

--- api/index.js
+++ api/index.js
@@ -1,25 +1,39 @@
 import express from "express";
-import postRoutes from "./routes/posts.js"
-import userRoutes from "./routes/users.js"
-import authRoutes from "./routes/auth.js"
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/users", userRoutes)
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
+const upload = multer({ storage });
 
-app.get("/test", (req,res)=>{
-  res.json("it works!")
-})
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file
+  res.status(200).json(file.filename);
+});
 
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
 
+app.get("/test", (req, res) => {
+  res.json("it works!");
+});
 
-app.listen(8800, ()=>{
-  console.log("Connected!")
-})
+app.listen(8800, () => {
+  console.log("Connected!");
+});
