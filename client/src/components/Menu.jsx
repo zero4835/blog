cfc2dc73848c77749cd251cc2a792008210ca6b7
Natsusaka <zz5120937@gmail.com>
2023-10-27 10:41:08 +0800

    feat: modifu img src

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -48,7 +48,7 @@ export const Menu = ({cat}) => {
       <h1>Other post you make like</h1>
       {posts?.map((post) => (
         <div className="post" key={post.id}>
-          <img src={post.img} alt="" />
+          <img src={`../upload/${post?.img}`} alt="" />
           <h2>{post.title}</h2>
           <button>read more</button>
         </div>
