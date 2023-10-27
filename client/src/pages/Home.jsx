9cd16361a38bc3009abad06a1baf8d535d6ca802
Natsusaka <zz5120937@gmail.com>
2023-10-27 10:43:14 +0800

    feat: modify img src

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -47,6 +47,11 @@ const Home = () => {
   //     img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   //   },
   // ];
+  
+  const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
 
   return (
     <div className="home">
@@ -54,13 +59,13 @@ const Home = () => {
         {posts?.map((post) => (
           <div className="post" key={post.id}>
             <div className="img">
-              <img src={post.img} alt={post.id} />
+              <img src={`../upload/${post.img}`} alt={post.id} />
             </div>
             <div className="content">
               <Link className="link" to={`/post/${post.id}`}>
                 <h1>{post.title}</h1>
               </Link>
-              <p>{post.desc}</p>
+              <p>{getText(post.desc)}</p>
               <button>Read More</button>
             </div>
           </div>
